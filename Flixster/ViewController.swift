bf1997b82f98a4ed3d52c8dfd2c53dd02951f226
Divya Sood <Divyasood2501@gmail.com>
2019-02-07 17:57:45 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Divya Sood on 2/7/19.
+//  Copyright © 2019 Divya Sood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
