20652e6faffd03b9ea15b10ef83acd143d53d703
Divya Sood <Divyasood2501@gmail.com>
2019-02-15 02:11:52 -0800

    Commit

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Divya Sood on 2/15/19.
+//  Copyright © 2019 Divya Sood. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
